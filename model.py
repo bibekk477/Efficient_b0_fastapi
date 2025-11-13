a540f2d0c77fb1e3bd517a847494fe6bebc3b6ac
Bibek Karki <bibekk477@gmail.com>
2025-11-13 14:55:34 +0545

    Added model.py

--- model.py
+++ model.py
@@ -1,14 +1,18 @@
-# model.py
 import torch
-import torchvision.models as models
+from torchvision import models
 import torch.nn as nn
 
 
-# Example: Using ResNet18 for simplicity
-def load_model(model_path="model.pth"):
-    model = models.resnet18(pretrained=False)
-    num_classes = 10  # Change according to your dataset
-    model.fc = nn.Linear(model.fc.in_features, num_classes)
+def load_model(model_path=None, NUM_CLASSES=None):
+    """
+    Load EfficientNet B0 with custom classifier for NUM_CLASSES
+    """
+    model = models.efficientnet_b0(weights=None)
+
+    # Replace classifier for your dataset
+    model.classifier[1] = nn.Linear(model.classifier[1].in_features, NUM_CLASSES)
+
+    # Load the trained weights
     model.load_state_dict(torch.load(model_path, map_location=torch.device("cpu")))
     model.eval()
     return model
