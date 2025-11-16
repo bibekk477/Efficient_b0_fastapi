c24d521bec1033e67acf96d28dbf933676b9cef7
Bibek Karki <bibekk477@gmail.com>
2025-11-16 12:28:39 +0545

    modified app.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,14 +1,16 @@
 from PIL import Image
 from torchvision import transforms
+import torchvision
 
 
 def preprocess_image(image: Image.Image):
     """
     Preprocesses an image for EfficientNet B0.
+    Returns a tensor with shape (1, C, H, W).
     """
     transform = transforms.Compose(
         [
-            transforms.Resize(224),
+            transforms.Resize(256),
             transforms.CenterCrop(224),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
